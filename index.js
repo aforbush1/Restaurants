401c48d9207a99ca819272ef6b43cd4fea517a47
aforbush1 <aforbush17@gmail.com>
2023-12-11 10:50:14 -0700

    Adam

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const knex = require("knex")({
     connection: {
         host: process.env.RDS_HOSTNAME || "localhost",
         user: process.env.USER_NAME || "postgres",
-        password: process.env.RDS_PASSWORD || "root1234",
+        password: process.env.RDS_PASSWORD || "root",
         database: process.env.RDS_DB_NAME || "restaurants",
         port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? {rejectUnauthorized:false}:false
